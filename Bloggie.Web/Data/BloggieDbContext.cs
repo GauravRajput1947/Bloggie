57da276b48d6f01ca43dbb000b965add9d7887a0
GauravRajput1947 <gauravchauhan6163@gmail.com>
2025-01-23 21:59:01 +0530

    Project Created

---
+++ Bloggie.Web/Data/BloggieDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Bloggie.Web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bloggie.Web.Data
+{
+    public class BloggieDbContext : DbContext
+    {
+        public BloggieDbContext(DbContextOptions options) : base(options)
+        {
+
+
+        }
+        public DbSet<BlogPost> BlogPost { get; set; }
+        public DbSet<Tag> Tag {get; set; }
+
+
+
+
+    }
+}
